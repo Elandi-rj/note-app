65be415c67c671f609b8a2b17474ec45db660466
elandi <56363232+Elandi-rj@users.noreply.github.com>
2021-03-28 20:14:20 +1100

    Updates

---
+++ precache-manifest.94d8d3ddaa446df89414a1a7acfe5071.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20c19fa7b59a50ab76f2e5077faabc1c",
+    "url": "/note-app/index.html"
+  },
+  {
+    "revision": "f064c7fcfc406533d388",
+    "url": "/note-app/static/css/2.d18c48e5.chunk.css"
+  },
+  {
+    "revision": "4d02a6dec040e1258fc9",
+    "url": "/note-app/static/css/main.6143f5b5.chunk.css"
+  },
+  {
+    "revision": "f064c7fcfc406533d388",
+    "url": "/note-app/static/js/2.5addf33c.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/note-app/static/js/2.5addf33c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d02a6dec040e1258fc9",
+    "url": "/note-app/static/js/main.dd9174d5.chunk.js"
+  },
+  {
+    "revision": "8a1a8e1c8cd7f2be9b78",
+    "url": "/note-app/static/js/runtime-main.7f39f972.js"
+  }
+]);
